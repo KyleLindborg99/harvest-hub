364c9aa61eb65c4941b02253a92b5f679b4bbadf
DESKTOP-766GD7Q\Kyle <azwerth@gmail.com>
2025-08-24 15:05:14 -0400

    change routes

--- my-frontend/src/App.tsx
+++ my-frontend/src/App.tsx
@@ -1,8 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
-import ShareLead from "./pages/ShareLead";
-import MemberLead from "./pages/MemberLead";
 import TokenFormPage from "./components/forms/TokenFormPage";
 import DebugTokenPage from "./components/forms/DebugTokenPage";
 
@@ -16,10 +14,6 @@ function App() {
         {/* Token-based form access (for QR codes) */}
         <Route path="/f/:token" element={<DebugTokenPage />} />
         <Route path="/form/:token" element={<TokenFormPage />} />
-
-        {/* Lead form pages (legacy/direct access) */}
-        <Route path="/lead/shares" element={<ShareLead />} />
-        <Route path="/lead/members" element={<MemberLead />} />
       </Routes>
     </BrowserRouter>
   );
